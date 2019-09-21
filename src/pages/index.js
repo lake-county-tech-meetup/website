cbea7a4f502f250033cc9c8d3066e1e4ba41f01c
Jacob Evans <cloud887@gmail.com>
2019-09-21 12:40:19 -0500

    changed some of the text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,10 @@ import SEO from "../components/seo";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Welcome to Lakewood Tech Meetup</h1>
+    <p>
+    Our group is attempting to bring the same level of quality and content as technology meetups you would find in downtown Chicago. We hope to foster mentorship, networking, as well as skills and career growth.
+    </p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
