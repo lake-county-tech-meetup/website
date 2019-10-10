5ebe0e9bea9ec83775e68347f1e0e7814a412b18
Stephy Miehle <stephy@blindingstars.com>
2019-10-09 21:47:25 -0500

    Init PostCSS & Tailwind

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,12 @@
 import { Link } from 'gatsby';
 import React from 'react';
 
-const headerNameStyle = {
-  color: `white`,
-  textDecoration: `none`,
-  marginRight: `2vw`,
-};
 const Header = () => (
-  <header
-    style={{
-      background: `darkcyan`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ display: `flex`, margin: 0, justifyContent: `flex-start` }}>
-        <Link
-          to="/"
-          style={{
-            ...headerNameStyle,
-          }}
-        >
-          Home
-        </Link>
-        <Link
-          to="/about-us/"
-          style={{
-            ...headerNameStyle,
-          }}
-        > About Us
-        </Link>
-      </h1>
+  <header>
+    <div className="container">
+      <h1></h1>
+      <Link to="/">Home</Link>
+      <Link to="/about-us/">About Us</Link>
     </div>
   </header>
 );
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      paddingLeft: '15px',
+      paddingRight: '15px',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
