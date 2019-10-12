df67b45c7f58c20d5fc2278086a469c1e873943b
Stephy Miehle <stephy@blindingstars.com>
2019-10-11 20:38:40 -0500

    Add basic page styles and nav system

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,69 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
-import React from 'react';
 
-const Header = () => (
-  <header>
-    <div className="container">
-      <h1></h1>
-      <Link to="/">Home</Link>
-      <Link to="/about-us/">About Us</Link>
-    </div>
-  </header>
+import logo from '../assets/images/logo.svg';
+
+const NavLink = ({ to, children }) => (
+  <li>
+    <Link
+      className="block font-bold text-primary hover:text-primary no-underline hover:no-underline py-1 md:p-1 md:border-b-4 md:ml-3 border-transparent sm:hover:border-secondary-100"
+      activeClassName="sm:border-secondary"
+      to={to}
+    >
+      {children}
+    </Link>
+  </li>
 );
+
+function Header({ siteTitle }) {
+  const [isExpanded, toggleExpansion] = useState(false);
+
+  return (
+    <header className="py-6 bg-white relative z-10 shadow">
+      <nav className="container flex flex-wrap items-center justify-between">
+        <Link to="/">
+          <img
+            src={logo}
+            alt={`${siteTitle} logo`}
+            className="w-48 md:w-64 hover:opacity-75"
+          />
+        </Link>
+        <div className="block md:hidden">
+          <button
+            onClick={() => toggleExpansion(!isExpanded)}
+            className="flex items-center px-3 py-2 border-2 rounded text-secondary border-secondary hover:text-secondary-700 hover:border-secondary-700"
+          >
+            <svg
+              className="fill-current h-3 w-4"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <ul
+          className={`${
+            isExpanded ? `block` : `hidden`
+          } w-full block flex-grow mt-4 md:mt-0 md:flex md:items-center md:w-auto md:justify-end`}
+        >
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about-us/">About Us</NavLink>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+};
+
+Header.defaultProps = {
+  siteTitle: ``,
+};
 
 export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -26,20 +26,22 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main className="md:bg-grey-100 md:py-10">
+        <div className="container bg-white p-8 md:rounded md:shadow">
+          {children}
+        </div>
+      </main>
+      <footer className="py-4">
+        <div className="container text-center text-grey-600">
+          © {new Date().getFullYear()} &middot; Built with{' '}
+          <a
+            href="https://www.gatsbyjs.org"
+            className="text-grey-800 no-underline"
+          >
+            Gatsby
+          </a>
+        </div>
+      </footer>
     </>
   );
 };
