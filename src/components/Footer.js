dd71d0778420584d340009c3675f5ab09edf4535
Jacob Evans <cloud887@gmail.com>
2019-10-12 22:28:40 -0500

    filesystem pathing casing needs to match import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
-const Footer = () => {
-  return (
-    <footer className="py-4 shadow">
-      <div className="container text-center text-grey-600">
-        © {new Date().getFullYear()} &middot; Built with{' '}
-        <a
-          href="https://www.gatsbyjs.org"
-          className="text-grey-800 no-underline"
-        >
-          Gatsby
-        </a>
-      </div>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="py-4 shadow">
+    <div className="container text-center text-grey-600">
+      © {new Date().getFullYear()} &middot; Built with
+      <a href="https://www.gatsbyjs.org" className="text-grey-800 no-underline">
+        Gatsby
+      </a>
+    </div>
+  </footer>
+);
 
 export default Footer;
