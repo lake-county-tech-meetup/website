c107d2d517dc0b892d5fa213cbc1d7a2f0c62b4d
Stephy Miehle <stephy@blindingstars.com>
2019-10-19 21:17:14 -0500

    Update config icon & colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,23 +4,23 @@ module.exports = {
     description: `A website for a local meetup in Lake County Illinois, all things
     technology, web, cloud, devOps, cybersecurity / infosec, blockchain, 
     @ThePracticalDev supporters ...etc.`,
-    author: `@jacobMGEvans`,
+    author: `@jacobMGEvans`
   },
   plugins: [
     `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/database/static`,
-      },
+        path: `${__dirname}/database/static`
+      }
     },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -30,16 +30,15 @@ module.exports = {
         name: `Lake County Tech Meetup`,
         short_name: `Lake Co. Tech`,
         start_url: `/`,
-        background_color: `#249`,
-        theme_color: `#249`,
+        background_color: `#003399`,
+        theme_color: `#23b573`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, //! Place Holder
-        // TODO: UPDATE IMAGE AND COLORS SCHEMA
-      },
+        icon: `src/assets/images/logo-circle.svg`
+      }
     },
-    `gatsby-plugin-postcss`,
+    `gatsby-plugin-postcss`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 };
