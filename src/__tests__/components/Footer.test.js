16e422f188032e6fa6ad352dfe6a749b00b0a9e8
Jacob Evans <cloud887@gmail.com>
2019-10-20 18:49:33 -0500

    Configured Jest tests for Gatsby to utilize babel-jest

--- src/__tests__/components/Footer.test.js
+++ src/__tests__/components/Footer.test.js
@@ -1,22 +1,20 @@
-import { render } from "@testing-library/react"
+import React from 'react';
+import { render } from '@testing-library/react';
 
-import Footer from '../../components/footer'
+import Footer from '../../components/footer';
 
 const setup = () => {
-
-  const utils = render(<Footer />)
+  const utils = render(<Footer />);
 
   return {
     ...utils,
-  }
+  };
+};
 
-}
+describe(`Footer Component`, () => {
+  test(`should render correct footer content`, () => {
+    const { getByText } = setup();
 
-describe('Footer Component', () => {
-
-  test('should render correct footer content', () => {
-    const { getByText } = setup()
-
-    expect(getByText('Gatsby')).toHaveTextContent('Gatsby')
-  })
-})
+    expect(getByText(`Gatsby`)).toHaveTextContent(`Gatsby`);
+  });
+});
