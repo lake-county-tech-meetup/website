f03efa80a68b774c41bed2e9352514850b40c9d4
Rob McFadzean <rob@northxsouth.co>
2019-10-22 14:28:33 -0500

    only show drafts in development

--- src/config/siteConfig.js
+++ src/config/siteConfig.js
@@ -1,7 +1,7 @@
 const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || `development`;
 
-const showDraft = activeEnv === `production`;
+const showDraft = activeEnv !== `production`;
 
 const events = {
   itemsPerPage: 2,
