33ed4e16bdc697c407fb0a0d28f5fa6f397b4864
Rob McFadzean <rob@northxsouth.co>
2019-10-27 15:33:52 -0500

    fixes gatsby failing to compile without events

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,9 @@
 const onCreateNode = require(`./gatsby/onCreateNode`);
 const onCreateWebpackConfig = require(`./gatsby/onCreateWebpackConfig`);
 const createPages = require(`./gatsby/createPages`);
+const createSchemaCustomization = require(`./gatsby/createSchemaCustomization`);
 
 exports.onCreateWebpackConfig = onCreateWebpackConfig;
 exports.onCreateNode = onCreateNode;
 exports.createPages = createPages;
+exports.createSchemaCustomization = createSchemaCustomization;
